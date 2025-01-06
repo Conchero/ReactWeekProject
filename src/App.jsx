23dfc3f3a00f820b5489c7fa096b372a1b82c26c
Conchero <florian.parra@hotmail.com>
2025-01-06 12:13:12 +0100

    simpson exercise

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,28 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Avatar from './Components/Avatar.jsx'
-import Hello from './Components/Hello.jsx'
-
+import Homer from './assets/img/Homer.png'
+import Bart from './assets/img/Bart.png'
 function App() {
   const [count, setCount] = useState(0)
+
+  const userInfoArray = [
+                    {firstName:"Homer",
+                      lastName:"Simpson",
+                    passion:"Drinking Duff",
+                    img:Homer},
+                    {firstName:"Bart",
+                      lastName:"Simpson",
+                      passion:"Making pranks",
+                      img:Bart}];
+
+  
+
 
   return (
     <>
       <div>
-        <Hello/>
-        <Avatar/>
+        {userInfoArray.map((el,i) => <Avatar key={i} infoUser={{...el}}/>)}
       </div>
     </>
   )
--- src/Components/Avatar.jsx
+++ src/Components/Avatar.jsx
@@ -1,8 +1,11 @@
-const Avatar = () => {
-    const name = "Joe";
-    return(
+import { useState } from "react";
+
+const Avatar = ({infoUser}) => {
+        return(
         <>
-        <h1>Hello this is {name}</h1>
+        <h1>Hello my name is {infoUser.firstName} {infoUser.lastName}</h1>
+        <img src={infoUser.img} alt="Picture of character"/>
+        <h2>I love {infoUser.passion}</h2>
         </>
     )
 
