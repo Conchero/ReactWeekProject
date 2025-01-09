7d14b4aad3fc5cc660e37b57c74cbfc8ff2a41c8
Conchero <florian.parra@hotmail.com>
2025-01-09 16:04:41 +0100

    rebase 2

---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+import MyRouter from './MyRouter.jsx'
+import { UserController } from './context/UsersContext.jsx'
+import { AuthController } from './context/AuthContext.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <UserController>
+      <AuthController>
+        <MyRouter />
+      </AuthController >
+    </UserController>
+  </BrowserRouter>,
+)
---
+++ src/pages/Weather.jsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from "react"
+import axios from 'axios'
+
+
+
+const Weather = () => {
+
+    const [city, setCity] = useState('Berlin')
+    const [result, setResult] = useState(null)
+    const [search, setSearch] = useState('Berlin')
+    const API_KEY = import.meta.env.VITE_WEATHER_API_KEY
+
+    const fetchWeather = async () => {
+        setCity(search)
+        try {
+            const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`)
+            setResult(response.data)
+        }
+        catch (err) {
+            console.error(err)
+        }
+    }
+
+    useEffect(() => {
+        fetchWeather()
+    }, [])
+
+    console.log(result)
+
+    return (
+        <>
+
+
+            <div className="flex flex-col justify-center items-center " >
+                <form onSubmit={(e) => {
+                    e.preventDefault()
+                    fetchWeather()
+                }}>
+                    <input type="text" onChange={e => setSearch(e.target.value)} />
+                    <input type="submit" value='search' />
+                </form>
+                <h1>Weather in {city} : {result && result.main.temp} degrees </h1>
+            </div>
+        </>
+    )
+}
+
+
+export default Weather
